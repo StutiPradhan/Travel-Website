e44d22efd7001009b46740b28e5ba6ece9a8a3f9
StutiPradhan <stutipradhan02@gmail.com>
2023-02-12 13:52:25 +0530

    Page 3 edited

--- ui/js/signin.js
+++ ui/js/signin.js
@@ -18,3 +18,18 @@ signupBtn.onclick=function()
     signupBtn.classList.remove("disable");
 
 }
+var username=document.getElementById(nextField).value;
+var emial=document.getElementById(emailHere).value;
+var password=document.getElementById(pwd).value;
+var filter=/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+ while((username>=a&&username<=z)||(username>=A&&username<=Z))
+ {
+     while((email>filter&&email<=filter))
+     {
+        if(password<6||password>6)
+        {
+            alert("Sign up successful");
+            window.location="index.html";
+        }
+     }
+ }
