b9e0a2ede091d4730afb6c1cdcd7d72e9ecfe440
StutiPradhan <stutipradhan02@gmail.com>
2023-04-30 15:56:58 +0530

    Changes made

--- ui/js/signin.js
+++ ui/js/signin.js
@@ -14,22 +14,37 @@ signupBtn.onclick=function()
 {
     nameField.style.maxHeight="6rem";
     title.innerHTML="Sign Up ";
-    signipBtn.classList.add("disable");
+    signinBtn.classList.add("disable");
     signupBtn.classList.remove("disable");
 
 }
-var username=document.getElementById(nextField).value;
-var emial=document.getElementById(emailHere).value;
-var password=document.getElementById(pwd).value;
+var username=document.getElementById("nextField").value;
+var emial=document.getElementById("emailHere").value;
+var password=document.getElementById("pwd").value;
 var filter=/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
- while((username>=a&&username<=z)||(username>=A&&username<=Z))
- {
-     while((email>filter&&email<=filter))
-     {
-        if(password<6||password>6)
-        {
-            alert("Sign up successful");
-            window.location="index.html";
-        }
-     }
- }
+if(username=''){
+    alert("Please enter user name.")
+}
+else if(password=''){
+    alert("Please enter a password")
+}
+else if(!filter.test(username)){
+    alert("Enter a valid email id.")
+}
+else if(password.length<6||password.length>6){
+    alert("Password min and max length is 6")
+}
+else{
+    alert("Sign In successfull");
+}
+// while((username>=a&&username<=z)||(username>=A&&username<=Z))
+//  {
+//      while((email>filter&&email<=filter))
+//      {
+//         if(password<6||password>6)
+//         {
+//             alert("Sign up successful");
+//             window.location="index.html";
+//         }
+//      }
+ //}
